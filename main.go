5af57f2b9ed161f5ebe1a1ce7053d7b148162ba1
akshaygajjala1 <akshay.gajjala1@gmail.com>
2026-06-29 20:54:33 -0500

    http start in go

--- main.go
+++ main.go
@@ -6,12 +6,29 @@ import (
 	"encoding/json"
 )
 
+type User struct {
+	Name string `json:"name`
+	ID int `json:"id`
+}	
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /read", readHandler)
+	mux.HandleFunc("GET /write", writeHandler)
+	http.ListenAndServe(":8080", mux)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request to: ", r.URL.Path)
-	fmt.Print("hello!")
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{Name: "test", ID: 1}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+	fmt.Print("write(get)")
 }
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	w.WriteHeader(http.StatusOK)
+	fmt.Print("read(post)")
+}
+
